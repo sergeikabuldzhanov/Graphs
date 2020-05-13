36e25fed79e1c9f313b888a9234c28a47c99dc40
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-05-13 19:17:39 +0500

    bop

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -3,7 +3,8 @@ from player import Player
 from world import World
 
 import random
-from collections import deque
+from collections import deque, defaultdict
+import heapq
 from ast import literal_eval
 
 # Load world
@@ -37,7 +38,7 @@ room_stack = deque()
 # room_stack.append()
 room_id_path = []
 # until we have visited all rooms
-while len(visited) < len(world.rooms):
+while len(visited) is not len(world.rooms):
     # traverse the graph depth fisrt
     # every time we change current node, add it to path
     # if the current node is not visited, mark it as visited
@@ -51,8 +52,7 @@ while len(visited) < len(world.rooms):
                  if visited[current_room.id][direction] == None]
     if len(unvisited):
         room_stack.append(current_room)
-        # move_direction = unvisited[0]
-        move_direction = random.choice(unvisited)
+        move_direction = unvisited[0]
         next_room = current_room.get_room_in_direction(move_direction)
         visited[current_room.id][move_direction] = next_room.id
         if next_room.id not in visited:
@@ -70,14 +70,93 @@ while len(visited) < len(world.rooms):
         traversal_path.append(direction)
         room_id_path.append(current_room.id)
 
-# world.print_rooms()
+graph = [[value for value in v.values() if value is not None]
+         for k, v in sorted(visited.items(), key=lambda x: x[0])]
+
+
+def bfs(starting_vertex, destination_vertex):
+    """
+    Return a list containing the shortest path from
+    starting_vertex to destination_vertex in
+    breath-first order.
+    """
+    visited = set()
+    visited.add(starting_vertex)
+    paths = deque()
+    paths.appendleft([starting_vertex])
+    # print(starting_vertex)
+    # store a queue of paths
+    while len(paths):
+        path = paths.pop()
+        node = path[-1]
+        # if the path ends with destination vertex, that the path we need, bfs ensures it's the shortest
+        if node == destination_vertex:
+            return path
+        # else, look at all the neghbours, and add new paths to the queue
+        else:
+            for adjacent in graph[node]:
+                if adjacent not in visited:
+                    visited.add(node)
+                    new_path = path+[adjacent]
+                    paths.appendleft(new_path)
+
+
+shortest_paths = [[bfs(starting_node, target_node)
+                   for target_node in range(len(graph))]for starting_node in range(len(graph))]
+# print(shortest_paths)
+
+def bfs_all_paths(graph, starting_vertex):
+    min_path_len = 99999999999
+    min_path = []
+    paths = deque()
+    # for every path we store the path and nodes visited so far
+    paths.appendleft(([starting_vertex], set([starting_vertex])))
+    # store a queue of paths
+    while len(paths):
+        path, visited = paths.pop()
+        # last node in a path is the one we are looking at
+        node = path[-1]
+        # if the path has visited all the nodes, done
+        if len(visited) == len(graph):
+            if len(path)< min_path_len:
+                min_path = path
+                min_path_len = len(path)
+                # print(min_path, min_path_len)
+            # return path
+        else:
+            # else, look at all the neghbours for the current node
+            unvisited = [n for n in graph[node] if n not in visited]
+            if unvisited:
+                for adjacent in unvisited:
+                    # if there are unvisited ones, add them to path and new path to queue
+                    new_path = path+[adjacent]
+                    new_visited = visited.copy()
+                    new_visited.add(adjacent)
+                    paths.appendleft((new_path, new_visited))
+            else:
+                # else if there are no unvisited nodes, backtrack until we find a node with
+                # unvisited neighbours 
+                # add the backtracking path to path
+                for node_id in reversed(path):
+                    unvisited = [n for n in graph[node_id] if n not in visited]
+                    if unvisited:
+                        path_back = shortest_paths[node][node_id][1::]
+                        for adjacent in unvisited:
+                            # if there are unvisited ones, add them to path and new path to queue
+                            new_path = path+path_back+[adjacent]
+                            new_visited = visited.copy()
+                            new_visited.add(adjacent)
+                            paths.appendleft((new_path, new_visited))
+    return min_path, min_path_len-1
+
+world.print_rooms()
+print("bfs smart",bfs_all_paths(graph, 0))
 # print(room_id_path)
 # print(traversal_path)
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
-
 for move in traversal_path:
     player.travel(move)
     visited_rooms.add(player.current_room)
---
+++ projects/adventure/adv_copy.py
@@ -0,0 +1,124 @@
+from room import Room
+from player import Player
+from world import World
+
+import random
+from ast import literal_eval
+from util import Stack, Queue
+from collections import defaultdict
+# Load world
+world = World()
+
+
+# You may uncomment the smaller graphs for development and testing purposes.
+# map_file = "maps/test_line.txt"
+# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_loop.txt"
+# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/main_maze.txt"
+
+# Loads the map into a dictionary
+room_graph=literal_eval(open(map_file, "r").read())
+world.load_graph(room_graph)
+
+# Print an ASCII map
+world.print_rooms()
+
+player = Player(world.starting_room)
+
+# Fill this out with directions to walk
+# traversal_path = ['n', 'n']
+traversal_path = []
+opposite = {
+    'n': 's', 
+    'e': 'w', 
+    's': 'n', 
+    'w': 'e'
+}
+# player.current_room.id
+# player.current_room.get_exits()
+# player.travel(direction)
+
+# when moving to another room (going south for example)
+# set previous room (south) to the id of current room
+# set current room oposite direction (north) to id of previous room
+
+def projected_path(starting_room, already_visited=set()):
+    visited = set()
+    # update visited
+    for room in already_visited: visited.add(room)
+    # hold path
+    path = []
+    def add_to_path(room, back=None):
+        # add room to visited
+        visited.add(room)
+        # get all room exits
+        exits = room.get_exits()
+        # for each exit
+        for direction in exits:
+            # if the room in that direction is not visited
+            if room.get_room_in_direction(direction) not in visited:
+                # add the direction to the path
+                path.append(direction)
+                # recursively add all the rooms in the same direction, while sending the way back
+                add_to_path(room.get_room_in_direction(direction), opposite[direction])
+        # if a way back exists, add it to the path
+        if back: path.append(back)
+    # start running at starting room
+    add_to_path(starting_room)
+    # return projected path
+    return path
+
+def create_path(starting_room, visited=set()):
+    # hold the path
+    path = []
+    def add_to_path(room, back=None):
+        # add room to visited
+        visited.add(room)
+        # get all room exits
+        exits = room.get_exits()
+        # hold all path projections
+        path_lengths = {}
+        # for each exit
+        for direction in exits:
+            # find projected path in that direction
+            path_lengths[direction] = len(projected_path(room.get_room_in_direction(direction), visited))
+        # hold the traverse order of the paths
+        traverse_order = []
+        # for each direction as projected path sorted by length of path
+        for direction, _ in sorted(path_lengths.items(), key = lambda x: x[1]): 
+            # add the direction to the traverse order
+            traverse_order.append(direction)
+        # for each direction in the traverse order
+        for direction in traverse_order:
+            # if the room in that direction was not visited
+            if room.get_room_in_direction(direction) not in visited:
+                # add direction to the path
+                path.append(direction)
+                # recursively add all the rooms in the same direction, while sending the way back
+                add_to_path(room.get_room_in_direction(direction), opposite[direction])
+        # if we've reached all rooms at least once, stop
+        if len(visited) == len(world.rooms): return
+        # else, go back and append to path
+        elif back: path.append(back)
+    # start running at starting room
+    add_to_path(starting_room)
+    # return path
+    return path
+
+traversal_path = create_path(world.starting_room)
+
+# TRAVERSAL TEST
+visited_rooms = set()
+player.current_room = world.starting_room
+visited_rooms.add(player.current_room)
+
+for move in traversal_path:
+    player.travel(move)
+    visited_rooms.add(player.current_room)
+
+if len(visited_rooms) == len(room_graph):
+    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+else:
+    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
